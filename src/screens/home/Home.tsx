caf3b3ad080b663ebd034ddb1b3f52449fea7402
akshPairroxz <56211500+aksh-22@users.noreply.github.com>
2023-01-11 22:56:08 +0530

    Update Home.tsx

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,12 +1,21 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {View} from 'react-native';
+import {useDispatch} from 'react-redux';
+import CustomButton from 'src/components/button/CustomButton';
+import {setIsLoggedIn} from 'src/store/reducer/userReducer';
 
 type Props = {};
 
-const Home = (props: Props) => {
+const Home = ({}: Props) => {
+  const dispatch = useDispatch();
+
+  const onPress = () => {
+    dispatch(setIsLoggedIn(false));
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{flex: 1, justifyContent: 'center', padding: 20}}>
+      <CustomButton title="logout" onPress={onPress} />
     </View>
   );
 };
