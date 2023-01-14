08590742dda61f197958c3cd2ce09d3bc82e3adc
akshPairroxz <56211500+aksh-22@users.noreply.github.com>
2023-01-14 23:00:14 +0530

    status bar color change

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View} from 'react-native';
 import CustomButton from 'src/components/button/CustomButton';
 import CustomHeader from 'src/components/header/CustomHeader';
+import colors from 'src/constants/colors';
 import Container from 'src/container/Container';
 import {BottomStackProps} from 'src/routes/types/navigation';
 import Deposit from './Deposit';
@@ -12,7 +13,10 @@ const Home = ({}: BottomStackProps) => {
   return (
     <>
       <CustomHeader heading="DCT" />
-      <Container contentContainerStyle={homeStyle.container}>
+      <Container
+        barStyle="light-content"
+        statusBarColor={colors.buttonColor1}
+        contentContainerStyle={homeStyle.container}>
         <CustomButton
           disabled
           title="Welcome To DCT"
