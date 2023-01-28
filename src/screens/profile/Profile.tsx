85ba997e5418f21ce36620170a8171d2f1270a71
akshPairroxz <56211500+aksh-22@users.noreply.github.com>
2023-01-28 23:29:30 +0530

    Update Profile.tsx

--- src/screens/profile/Profile.tsx
+++ src/screens/profile/Profile.tsx
@@ -4,10 +4,14 @@ import CustomInput from 'src/components/Input/CustomInput';
 import globalStyles from 'src/constants/globalStyles';
 import Container from 'src/container/Container';
 import profileStyle from './profile.style';
+import {useAppSelector} from 'src/utils/reducer';
 
 type Props = {};
 
 const Profile = ({}: Props) => {
+  const user = useAppSelector(state => state.userReducer.user);
+  console.log('user', JSON.stringify(user, null, 2));
+
   return (
     <>
       <CustomHeader isBack heading="Profile" />
@@ -16,21 +20,29 @@ const Profile = ({}: Props) => {
           mainContainerStyle={profileStyle.input}
           showLabel
           label="Name"
+          value={user.name}
+          editable={false}
         />
         <CustomInput
           mainContainerStyle={profileStyle.input}
           showLabel
-          label="UserName"
+          label="Email"
+          value={user.email}
+          editable={false}
         />
         <CustomInput
           mainContainerStyle={profileStyle.input}
           showLabel
           label="Mobile"
+          value={user.mobile}
+          editable={false}
         />
         <CustomInput
           mainContainerStyle={profileStyle.input}
           showLabel
           label="Referral Code"
+          value={user.name}
+          editable={false}
         />
       </Container>
     </>
