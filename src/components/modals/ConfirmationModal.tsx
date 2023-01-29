ee875c0b47e8b42227dbc316514d7a39990a760a
akshPairroxz <56211500+aksh-22@users.noreply.github.com>
2023-01-29 23:03:13 +0530

    Create ConfirmationModal.tsx

---
+++ src/components/modals/ConfirmationModal.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const ConfirmationModal = ({}: Props) => {
+  return (
+    <View>
+      <Text>ConfirmationModal</Text>
+    </View>
+  );
+};
+
+export default ConfirmationModal;
