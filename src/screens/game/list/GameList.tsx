a97f8c6b0b1d5ee1356c7486ccbba2de4bdd8198
akshPairroxz <56211500+aksh-22@users.noreply.github.com>
2023-02-22 23:17:20 +0530

    Update GameList.tsx

--- src/screens/game/list/GameList.tsx
+++ src/screens/game/list/GameList.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import CustomHeader from 'src/components/header/CustomHeader';
-import SvgBackground from 'src/components/SvgBackground';
 import {useRequest} from 'src/constants/useRequest';
 import Container from 'src/container/Container';
 import {gameDataType} from 'typings/game-type';
