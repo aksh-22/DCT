2d26516c26f81c714ed21f6a42c5713e2f45b9b3
akshPairroxz <56211500+aksh-22@users.noreply.github.com>
2023-06-20 23:18:53 +0530

    header

--- src/components/header/CustomHeader.tsx
+++ src/components/header/CustomHeader.tsx
@@ -1,7 +1,7 @@
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import React from 'react';
-import {TouchableOpacity, View} from 'react-native';
+import {SafeAreaView, TouchableOpacity, View} from 'react-native';
 import colors from 'src/constants/colors';
 import globalStyles, {margin} from 'src/constants/globalStyles';
 import {AuthorizeStackName} from 'src/constants/routeNames';
@@ -42,66 +42,70 @@ const CustomHeader = ({
   };
 
   return (
-    <View style={headerStyle.wrapper}>
-      <View style={headerStyle.header}>
-        {leftSmallTitle || leftTitle ? (
-          <View>
-            {leftSmallTitle ? (
-              <CustomText size={13}>{leftSmallTitle}</CustomText>
+    <SafeAreaView>
+      <View style={headerStyle.wrapper}>
+        <View style={headerStyle.header}>
+          {leftSmallTitle || leftTitle ? (
+            <View>
+              {leftSmallTitle ? (
+                <CustomText size={13}>{leftSmallTitle}</CustomText>
+              ) : null}
+              {leftTitle ? (
+                <CustomText size={30}>{leftTitle}</CustomText>
+              ) : null}
+            </View>
+          ) : null}
+          {isBack ? (
+            <TouchableOpacity
+              onPress={goBack}
+              style={headerStyle.backIconWrapper}>
+              <CustomIcon name="back" color={colors.defaultWhite} />
+            </TouchableOpacity>
+          ) : null}
+          <View style={headerStyle.rightIconArea}>
+            {showWallet ? (
+              <TouchableOpacity
+                onPress={onWalletIconPress}
+                style={headerStyle.icon}>
+                <CustomIcon
+                  name="withdrawal"
+                  fill={colors.defaultBlack}
+                  height={20}
+                  width={20}
+                />
+                <CustomText size={10} color="black" style={margin(0, 0, 5)}>
+                  {user?.wallet}
+                </CustomText>
+              </TouchableOpacity>
             ) : null}
-            {leftTitle ? <CustomText size={30}>{leftTitle}</CustomText> : null}
+            {showBell ? (
+              <TouchableOpacity onPress={onBellIconPress}>
+                <CustomIcon
+                  name="bell"
+                  color={colors.defaultWhite}
+                  height={32}
+                  width={32}
+                />
+              </TouchableOpacity>
+            ) : null}
+          </View>
+        </View>
+        {heading ? (
+          <View style={globalStyles.row_spaceBetween}>
+            <CustomText size={30} style={margin(20, 20, 20)}>
+              {heading}
+            </CustomText>
+            {status && (
+              <View style={headerStyle.status}>
+                <CustomText size={11} color="green">
+                  Open
+                </CustomText>
+              </View>
+            )}
           </View>
         ) : null}
-        {isBack ? (
-          <TouchableOpacity
-            onPress={goBack}
-            style={headerStyle.backIconWrapper}>
-            <CustomIcon name="back" color={colors.defaultWhite} />
-          </TouchableOpacity>
-        ) : null}
-        <View style={headerStyle.rightIconArea}>
-          {showWallet ? (
-            <TouchableOpacity
-              onPress={onWalletIconPress}
-              style={headerStyle.icon}>
-              <CustomIcon
-                name="withdrawal"
-                fill={colors.defaultBlack}
-                height={20}
-                width={20}
-              />
-              <CustomText size={10} color="black" style={margin(0, 0, 5)}>
-                {user?.wallet}
-              </CustomText>
-            </TouchableOpacity>
-          ) : null}
-          {showBell ? (
-            <TouchableOpacity onPress={onBellIconPress}>
-              <CustomIcon
-                name="bell"
-                color={colors.defaultWhite}
-                height={32}
-                width={32}
-              />
-            </TouchableOpacity>
-          ) : null}
-        </View>
       </View>
-      {heading ? (
-        <View style={globalStyles.row_spaceBetween}>
-          <CustomText size={30} style={margin(20, 20, 20)}>
-            {heading}
-          </CustomText>
-          {status && (
-            <View style={headerStyle.status}>
-              <CustomText size={11} color="green">
-                Open
-              </CustomText>
-            </View>
-          )}
-        </View>
-      ) : null}
-    </View>
+    </SafeAreaView>
   );
 };
 
